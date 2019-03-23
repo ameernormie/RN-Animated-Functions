3701b8f354048a8406649ddda1cea9118237a055
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-03-23 10:44:13 +0500

    Loop animation function applied to animations

--- screens/Interpolate/Interpolate.js
+++ screens/Interpolate/Interpolate.js
@@ -3,57 +3,25 @@ import { Animated, View, TouchableWithoutFeedback } from "react-native";
 
 class Interpolate extends React.Component {
   state = {
-    animation: new Animated.Value(0),
     rotationAnimation: new Animated.Value(0)
   };
 
-  startAnimation = () => {
-    Animated.timing(this.state.animation, {
-      toValue: 1,
-      duration: 1500
-    }).start(() => {
-      Animated.timing(this.state.animation, {
-        toValue: 0,
-        duration: 1500
-      }).start();
-    });
-  };
 
   startRotationAnimation = () => {
-    Animated.timing(this.state.rotationAnimation, {
-      toValue: 360,
+    Animated.loop(Animated.timing(this.state.rotationAnimation, {
+      toValue: 1,
       duration: 1500
-    }).start(() => {
-      Animated.timing(this.state.rotationAnimation, {
-        toValue: 0,
-        duration: 1500
-      }).start();
-    });
+    })).start();
   };
 
   render() {
-    const { animation, rotationAnimation } = this.state;
-    const boxInterpolation = animation.interpolate({
-      inputRange: [0, 1],
-      outputRange: ["rgb(255,99,71)", "rgb(99,71,255)"]
-    });
+    const { rotationAnimation } = this.state;
 
-    const colorInterpolation = animation.interpolate({
-      inputRange: [0, 1],
-      outputRange: ["rgb(99,71,255)", "rgb(255,99,71)"]
-    });
 
     const rotateInterpolation = rotationAnimation.interpolate({
-      inputRange: [0, 360],
+      inputRange: [0, 1],
       outputRange: ["0deg", "360deg"]
     });
-
-    const boxAnimatedStyle = {
-      backgroundColor: boxInterpolation
-    };
-    const textColorStyle = {
-      color: colorInterpolation
-    };
 
     const rotateAnimationStyle = {
       transform: [
@@ -71,23 +39,6 @@ class Interpolate extends React.Component {
           alignItems: "center"
         }}
       >
-        <TouchableWithoutFeedback onPress={this.startAnimation}>
-          <Animated.View
-            style={[
-              {
-                height: 100,
-                width: 100,
-                alignItems: "center",
-                justifyContent: "center"
-              },
-              boxAnimatedStyle
-            ]}
-          >
-            <Animated.Text style={[textColorStyle]}>
-              Color Interpolation
-            </Animated.Text>
-          </Animated.View>
-        </TouchableWithoutFeedback>
         {/* Rotation Interpolation */}
         <TouchableWithoutFeedback onPress={this.startRotationAnimation}>
           <Animated.View
@@ -102,7 +53,7 @@ class Interpolate extends React.Component {
               rotateAnimationStyle
             ]}
           >
-            <Animated.Text style={[textColorStyle]}>
+            <Animated.Text >
               Rotation Interpolation
             </Animated.Text>
           </Animated.View>
